20290e34b4cfc73579ee73ad541a1dc5ff38dc63
Nivedin <nivedinp@gmail.com>
2020-05-30 13:15:17 +0530

    added sapne logo and footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,14 +1,24 @@
 import React from "react";
 import styles from "./Footer.module.css";
+import FacebookIcon from "@material-ui/icons/Facebook";
+import InstagramIcon from "@material-ui/icons/Instagram";
+import YouTubeIcon from "@material-ui/icons/YouTube";
 
 function Navbar() {
   return (
     <div>
       <div className={styles.footer}>
-        <a
-          href="https://nivedin.github.io/MyPortfolio/">
-          Nivedin(2020)
-        </a>
+        <h6 className={styles.sapne}>
+          <a href="http://sapne.org.in/"> SAPNE NGO</a>
+        </h6>
+        <p className={styles.name}>
+          <a href="https://nivedin.github.io/MyPortfolio/">Nivedin(2020)</a>
+        </p>
+        <div className={styles.icons}>
+          <a href="https://www.facebook.com/Sapnethedreams/"><FacebookIcon /></a>
+          <a href="https://instagram.com/sapnethedreams?igshid=itt3vkizw5p4"><InstagramIcon /></a>
+          <a href="https://www.youtube.com/channel/UCZUECOx6Z66xnUjVEi57fSw"><YouTubeIcon /></a>
+        </div>
       </div>
     </div>
   );
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,19 +1,20 @@
 import React from "react";
 
 import styles from "./Navbar.module.css";
+import logo from "./sapnelogo-2.png";
 
 function Navbar() {
   return (
     <div>
       <nav className={styles.navbar}>
-      <span></span>
+      <span><a href="http://sapne.org.in/"><img src={logo} alt="sapneLogo" height={55}/></a></span>
         <div className={styles.title}>
           <a href="/">Covid Tracker</a>
         </div>
         <div className={styles.items}>
           <ul>
             <li>
-              <a href="/">HOME</a>
+              <a href="http://sapne.org.in/">HOME</a>
             </li>
             <li>
               <a href="https://covid-19-trackernp.netlify.app">GLOBAL</a>
